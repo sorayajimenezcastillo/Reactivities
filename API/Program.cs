1456afb384a0c48d213aee50ef19e04646ca8565
Soraya Jimenez <soraya.jimenez.castillo@akersolutions.com>
2020-07-29 10:37:31 +0200

    Initial commit

---
+++ API/Program.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            // This will create the DB defined in the Persintance/Migrations files.
+            using (var scope = host.Services.CreateScope()) 
+            {
+                var services = scope.ServiceProvider;  
+                try
+                {
+                    var context = services.GetRequiredService<DataContext>();
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<Logger<Program>>();
+                    logger.LogError(ex, "An error occured during migration");
+                }  
+            }
+
+            host.Run(); // Run applictaion
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
