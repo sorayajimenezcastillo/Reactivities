9cf6ab19f2bbe5a4b16eee2fc3141c6ca8e3bc2a
Soraya Jimenez <sorayajimenezcastillo@gmail.com>
2020-07-30 10:58:14 +0200

    Added Create, Edit, Delete handlers. End of section 4.

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Application.Activities;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace API
@@ -25,6 +20,7 @@ namespace API
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        // Dependency injector container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -39,6 +35,7 @@ namespace API
                     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
                 });
             });
+            services.AddMediatR(typeof(List.Handler).Assembly); // Tell mediator where our handlers are. 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
