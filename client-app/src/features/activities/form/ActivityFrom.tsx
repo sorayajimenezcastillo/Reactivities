44cc682a1d0b9fb7ac0651341cc80a7d0d8b289e
Soraya Jimenez <sorayajimenezcastillo@gmail.com>
2020-07-31 13:56:40 +0200

    Added Axios on client. End of section 6

--- client-app/src/features/activities/form/ActivityFrom.tsx
+++ client-app/src/features/activities/form/ActivityFrom.tsx
@@ -8,12 +8,14 @@ interface IProps {
   activity: IActivity | null;
   createActivity: (activity: IActivity) => void;
   editActivity: (activity: IActivity) => void;
+  submitting: boolean;
 }
 export const ActivityFrom: React.FC<IProps> = ({
   setEditMode,
   activity: initialFormState,
   createActivity,
-  editActivity
+  editActivity,
+  submitting
 }) => {
   const initializeForm = () => {
     if (initialFormState) {
@@ -90,7 +92,7 @@ export const ActivityFrom: React.FC<IProps> = ({
           name="venue"
           value={activity.venue}
         />
-        <Button floated="right" positive type="submit" content="Submit" />
+        <Button loading={submitting} floated="right" positive type="submit" content="Submit" />
         <Button
           onClick={() => setEditMode(false)}
           floated="right"
